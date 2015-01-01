4d0ad5e838d8f075447c559c395a505e1633bf43
hs105133 <hemant.singh@techmahindra.com>
2015-01-01 09:56:00 +0530

    my 1st commit

---
+++ production.js
@@ -0,0 +1,29 @@
+var deployd = require('deployd');
+
+var server = deployd({
+  port: process.env.PORT || 5000,
+  env: 'development',
+  db: {
+    host: 'ds027751.mongolab.com',
+    port: 27751, 
+    name: 'dh',
+    credentials: {
+      username: 'hemant',
+      password: '123'
+    }
+  }
+});
+
+server.listen();
+
+server.on('listening', function() {
+  console.log("Server is listening on " + process.env.PORT || 5000);
+});
+
+server.on('error', function(err) {
+  process.nextTick(function() { // Give the server a chance to return an error
+    process.exit();
+  });
+});
+
+
