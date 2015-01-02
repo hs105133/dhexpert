fb668359ed62cb4057d383df82af4618c15f3694
hs105133 <hemant.singh@techmahindra.com>
2015-01-02 14:58:07 +0530

    update button, search filter added 1

--- production.js
+++ production.js
@@ -3,20 +3,20 @@ var deployd = require('deployd');
 var server = deployd({
   port: process.env.PORT || 5000,
   env: 'development',
-  // db: {
-  //   host: 'ds027751.mongolab.com',
-  //   port: 27751, 
-  //   name: 'dh',
-  //   credentials: {
-  //     username: 'hemant',
-  //     password: '123'
-  //   }
-  // }
   db: {
-    host: 'localhost',
-    port: 27017, 
-    name: 'test'
+    host: 'ds027751.mongolab.com',
+    port: 27751, 
+    name: 'dh',
+    credentials: {
+      username: 'hemant',
+      password: '123'
+    }
   }
+  // db: {
+  //   host: 'localhost',
+  //   port: 27017, 
+  //   name: 'test'
+  // }
 });
 
 server.listen();
